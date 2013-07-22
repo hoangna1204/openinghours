82a5ec5be9152fb53c88a5ceef69c52d1caca750
ndthanh <ndt181@gmail.com>
2013-07-22 21:55:40 +0200

    jade

--- routes/dev.js
+++ routes/dev.js
@@ -1,3 +1,4 @@
 exports.testJade = function(req, res){
-  res.render('record_create', { hoang: 'Hello you' });
+  // res.render('record_create', { hoang: 'Hello you' });
+  res.render('record_details', { hoang: 'Hello you' });
 };
