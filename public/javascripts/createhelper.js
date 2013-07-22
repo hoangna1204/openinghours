c41ad635dab7fd437a8675d54d0c15a122a11c22
ndthanh <ndt181@gmail.com>
2013-07-22 15:01:36 +0200

    new name for create3.jade

--- public/javascripts/createhelper.js
+++ public/javascripts/createhelper.js
@@ -1,35 +1,43 @@
-
+var limit = 3;
+$(function() {
+      $(document).on("click", ".j-add-interval", function (event) {
+          event.preventDefault();
+          var dayHours = $(this).closest('.j-day-hours');
+          var hoursContainer = $(this).closest('td');
+          // limit time interval to only 3
+          if(hoursContainer.children().length < limit)
+              dayHours.clone().appendTo(hoursContainer).find('input').val('');
+          else
+              console.log('over3');
+      });
+});
 $(document).on("click", ".j-get-from-prev-day", function (event) {
       var days = [
-            {id: 'mon', name: 'Montag'},
-            {id: 'tue', name: 'Dienstag'},
-            {id: 'wed', name: 'Mittwoch'},
-            {id: 'thu', name: 'Donnerstag'},
-            {id: 'fri', name: 'Freitag'},
-            {id: 'sat', name: 'Samstag'},
-            {id: 'sun', name: 'Sonntag'}
+            {id: 'mon', name: 'Thứ 2'},
+            {id: 'tue', name: 'Thứ 3'},
+            {id: 'wed', name: 'Thứ 4'},
+            {id: 'thu', name: 'Thứ 5'},
+            {id: 'fri', name: 'Thứ 6'},
+            {id: 'sat', name: 'Thứ 7'},
+            {id: 'sun', name: 'CN'}
         ];
-            event.preventDefault();
-            var btn = $(this);
-            var day = $(this).closest('tr').data('weekday');
-            for (var dayIndex = 0; dayIndex < days.length; dayIndex++) {
-                if (days[dayIndex].id == day) {
-                    break;
-                }
+        event.preventDefault();
+        var btn = $(this);
+        var day = $(this).closest('tr').data('weekday');
+        for (var dayIndex = 0; dayIndex < days.length; dayIndex++) {
+            if (days[dayIndex].id == day) {
+                break;
             }
-            // var hoursForm = btn.closest('.j-season-container');
-            var sourceDay = days[dayIndex - 1].id;
-            var sourceDayRow = $('tr[data-weekday=' + sourceDay + ']');
-            var sourceDayHours = $('.j-day-hours', sourceDayRow);
-            var targetDayHours = $(this).closest('tr').find('.j-day-hours');
-            var targetContainer = targetDayHours.closest('td');
-            targetDayHours.each(function () {
-                $(this).remove();
-            });
-            sourceDayHours.clone(true).appendTo(targetContainer);
-
-
-
+        }
+        var sourceDay = days[dayIndex - 1].id;
+        var sourceDayRow = $('tr[data-weekday=' + sourceDay + ']');
+        var sourceDayHours = $('.j-day-hours', sourceDayRow);
+        var targetDayHours = $(this).closest('tr').find('.j-day-hours');
+        var targetContainer = targetDayHours.closest('td');
+        targetDayHours.each(function () {
+            $(this).remove();
+        });
+        sourceDayHours.clone(true).appendTo(targetContainer);
 });
 $(document).on("click", ".j-set-closed", function (event) {
             event.preventDefault();
